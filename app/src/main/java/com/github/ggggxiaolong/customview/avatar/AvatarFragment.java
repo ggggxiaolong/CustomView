828d2eba7b69b9d4a70f8189f788544eda59cd2f
MrTan <1120610800@qq.com>
2016-08-27 20:28:13 +0800

    添加使用自定义drawable未完成圆角头像

--- app/src/main/java/com/github/ggggxiaolong/customview/avatar/AvatarFragment.java
+++ app/src/main/java/com/github/ggggxiaolong/customview/avatar/AvatarFragment.java
@@ -1,11 +1,14 @@
 package com.github.ggggxiaolong.customview.avatar;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.github.ggggxiaolong.customview.R;
 
@@ -14,6 +17,10 @@ public final class AvatarFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_avatar,container,false);
+        View view = inflater.inflate(R.layout.fragment_avatar, container, false);
+        ImageView image = (ImageView) view.findViewById(R.id.image);
+        Bitmap bitmap = BitmapFactory.decodeResource(container.getResources(), R.drawable.nav_header);
+        image.setImageDrawable(new AvatarImageDrawable(bitmap));
+        return view;
     }
 }
---
+++ app/src/main/java/com/github/ggggxiaolong/customview/avatar/AvatarImageDrawable.java
@@ -0,0 +1,61 @@
+package com.github.ggggxiaolong.customview.avatar;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+
+/**
+ * 张鸿洋的博客
+ * http://blog.csdn.net/lmj623565791/article/details/43752383
+ */
+public final class AvatarImageDrawable extends Drawable {
+
+    private BitmapShader mShader;
+    private int mWidth;
+    private Paint mPaint;
+
+    public AvatarImageDrawable(Bitmap bitmap) {
+        mShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        mWidth = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setShader(mShader);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawCircle(mWidth / 2, mWidth / 2, mWidth / 2, mPaint);
+    }
+
+    @Override
+    public void setAlpha(int i) {
+        mPaint.setAlpha(i);
+    }
+
+    //当设置的值为Wrap_content时返回bitmap的宽度
+    @Override
+    public int getIntrinsicWidth() {
+        return mWidth;
+    }
+
+    //当设置的值为Wrap_content时返回bitmap的宽度
+    @Override
+    public int getIntrinsicHeight() {
+        return mWidth;
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+        mPaint.setColorFilter(colorFilter);
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+}
